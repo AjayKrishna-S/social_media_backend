f2bae079c01844190487ac9a76a784fd22bfd345
AjayKrishna-S <ajaykrishna.sak@gmail.com>
2024-07-23 13:03:14 +0530

    change the client path

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use((req,res,next)=>{
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:"http://localhost:3000",
+    origin:"https://669f5683c56413d28b48a358--socialmeetup.netlify.app",
     credentials: true
 }))
 
